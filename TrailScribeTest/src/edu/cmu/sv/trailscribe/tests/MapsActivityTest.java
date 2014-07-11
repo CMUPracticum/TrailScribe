9640d829db18e456dd837a93e425f5e19c408aab
Jerome Rasky <jyrome.112@gmail.com>
2014-07-11 11:40:47 -0700

    Implement tests for js interface functions instead of going through the JS itself
    Major code cleanup

--- TrailScribeTest/src/edu/cmu/sv/trailscribe/tests/MapsActivityTest.java
+++ TrailScribeTest/src/edu/cmu/sv/trailscribe/tests/MapsActivityTest.java
@@ -2,23 +2,12 @@ package edu.cmu.sv.trailscribe.tests;
 
 import android.test.ActivityInstrumentationTestCase2;
 import edu.cmu.sv.trailscribe.view.MapsActivity;
-import edu.cmu.sv.trailscribe.controller.MapsController;
 import android.webkit.WebView;
 import android.test.UiThreadTest;
-import android.webkit.ValueCallback;
 import java.util.concurrent.CountDownLatch;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.io.InputStream;
 import android.widget.Button;
 import android.util.Log;
-import android.webkit.ConsoleMessage;
-import android.webkit.WebChromeClient;
 import java.util.concurrent.TimeUnit;
-import android.webkit.WebViewClient;
-import android.location.Location;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
@@ -27,57 +16,34 @@ import android.os.SystemClock;
 import edu.cmu.sv.trailscribe.R;
 import android.test.RenamingDelegatingContext;
 import edu.cmu.sv.trailscribe.dao.DBHelper;
-import android.view.LayoutInflater;
-import android.content.Context;
-import android.content.ContextWrapper;
+import edu.cmu.sv.trailscribe.dao.LocationDataSource;
+import edu.cmu.sv.trailscribe.dao.SampleDataSource;
+import edu.cmu.sv.trailscribe.model.Sample;
+import java.util.List;
+import android.location.Location;
 
 public class MapsActivityTest extends ActivityInstrumentationTestCase2<MapsActivity> {
     private static final String WEBVIEW_URL = "file:///android_asset/map.html";
     private static final boolean JAVASCRIPT_ENABLED = true;
     private static final String LOG_TAG = "MapsActivityTest";
-    private static final long PAGE_LOAD_TIMEOUT = 50L;
     private static final long GENERAL_AWAIT_TIMEOUT = 10L;
     private static final long IDLE_SYNC_TIMEOUT = 10L;
-    private static final String SAMPLE_POINT_LOCATIONS = "\"[{\\\"x\\\":-13585932.705423407,\\\"y\\\":4497531.859545275},{\\\"x\\\":-13589272.29014674,\\\"y\\\":4497531.859545275},{\\\"x\\\":-13592611.874870075,\\\"y\\\":4494729.00634739}]\"";
-    private static final String CURRENT_LOCATION_JSON = "\"[{\\\"x\\\":-13590658.885723868,\\\"y\\\":4491810.069464362}]\"";
-    private static final String POSITION_HISTORY = "\"[{\\\"x\\\":-13586526.149628745,\\\"y\\\":4495410.731525376},{\\\"x\\\":-13586683.889347177,\\\"y\\\":4495850.197979055},{\\\"x\\\":-13586979.999192646,\\\"y\\\":4496576.422973805},{\\\"x\\\":-13586963.301269028,\\\"y\\\":4496872.561169761},{\\\"x\\\":-13586976.103010466,\\\"y\\\":4497270.041372993}]\"";
+    private static final String SAMPLES_RESULT = "{\"points\":[{\"y\":\"37.418\",\"x\":\"-122.04451\"},{\"y\":\"1.0\",\"x\":\"1.0\"},{\"y\":\"2.0\",\"x\":\"2.0\"},{\"y\":\"37.418\",\"x\":\"-122.04451\"}]}";
+    private static final String CURRENT_LOCATION_RESULT = "{\"points\":[{\"y\":\"1.0\",\"x\":\"1.0\"}]}";
+    private static final String POSITION_HISTORY_RESULT = "{\"points\":[{\"y\":\"2\",\"x\":\"2\"},{\"y\":\"3\",\"x\":\"3\"}]}";
     private static final String LOC_PROVIDER = "flp";
-    private static final double LOC_LAT = 37.377166;
-    private static final double LOC_LNG = -122.086966;
-    private static final float LOC_ACCURACY = 3.0f;
     private static final String TEST_FILE_PREFIX = "test_";
 
     private LocationClient tLocationClient;
 
     private MapsActivity tMapsActivity;
     private WebView tWebView;
-    private MapsController tMapsController;
 
     private DBHelper tHelper;
 
     private Button tSamplesButton;
     private Button tCurrentLocationButton;
     private Button tPositionHistoryButton;
-
-    private class ResultContainer<T> {
-        private T result;
-
-        public ResultContainer() {
-            result = null;
-        }
-
-        public ResultContainer(T initial) {
-            result = initial;
-        }
-
-        public T getResult() {
-            return result;
-        }
-
-        public void setResult(T value) {
-            result = value;
-        }
-    }
 
     public MapsActivityTest() {
         super("edu.cmu.sv.trailscribe", MapsActivity.class);
@@ -98,20 +64,6 @@ public class MapsActivityTest extends ActivityInstrumentationTestCase2<MapsActiv
             .findViewById(R.id.maps_current_location);
         tPositionHistoryButton = (Button) tMapsActivity
             .findViewById(R.id.maps_position_history);
-
-        // set a web chrome client with more debug info
-        tMapsActivity.runOnUiThread(new Runnable() {
-                public void run() {
-                    tWebView.setWebChromeClient(new WebChromeClient() {
-                            public boolean onConsoleMessage(ConsoleMessage cm) {
-                                Log.d("MapsActivity", cm.message() + " -- From line "
-                                      + cm.lineNumber() + " of "
-                                      + cm.sourceId() );
-                                return true;
-                            }
-                        });
-                }
-            });
 
         // setup location
         tLocationClient = new LocationClient(tMapsActivity, tMapsActivity, tMapsActivity);
@@ -151,69 +103,6 @@ public class MapsActivityTest extends ActivityInstrumentationTestCase2<MapsActiv
         return newLocation;
     }
 
-    private boolean waitForWebView() throws InterruptedException {
-        // wait for the webview to finish loading
-        // Why sleep for 3 seconds?
-        // Because otherwise things break
-        Thread.sleep(3000);
-        final CountDownLatch webviewDone = new CountDownLatch(1);
-        tMapsActivity.runOnUiThread(new Runnable() {
-                public void run() {
-                    final String url = tWebView.getUrl();
-                    Log.d(LOG_TAG, "Page currently at " + url);
-                    if (tWebView.getProgress() == 100 && url.equals(WEBVIEW_URL)) {
-                        webviewDone.countDown();
-                    } else {
-                        tWebView.setWebViewClient(new WebViewClient() {
-                                public void onPageFinished(WebView view, String url) {
-                                    Log.d(LOG_TAG, "Finished loading " + url);
-                                    if (url.equals(WEBVIEW_URL)) {
-                                        webviewDone.countDown();
-                                    }
-                                }
-                            });
-                    }
-                }
-            });
-        try {
-            final boolean result = webviewDone.await(PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
-            return result;
-        } catch (InterruptedException e) {
-            return false;
-        }
-    }
-
-    private String readResource(final String fname) throws UnsupportedEncodingException, IOException {
-        final StringBuilder buf = new StringBuilder();
-        final InputStream file = getInstrumentation().getContext().getAssets().open(fname);
-        final BufferedReader in = new BufferedReader(new InputStreamReader(file, "UTF-8"));
-        String str;
-        while ((str = in.readLine()) != null) {
-            buf.append(str + "\n");
-        }
-        in.close();
-
-        return buf.toString();
-    }
-
-    private void runJsOnUiThread(final ResultContainer container, final String js) throws Throwable {
-        final CountDownLatch done = new CountDownLatch(1);
-        runTestOnUiThread(new Runnable() {
-                public void run() {
-                    tWebView.evaluateJavascript(js, new ValueCallback<String>() {
-                            public void onReceiveValue(String value) {
-                                Log.d(LOG_TAG, "Value received: " + value);
-                                container.setResult(value);
-                                done.countDown();
-                            }
-                        });
-                    Log.d(LOG_TAG, "JS started");
-                }
-            });
-        Log.d(LOG_TAG, "Waiting for result");
-        assertTrue("JS execution timed out", done.await(GENERAL_AWAIT_TIMEOUT, TimeUnit.SECONDS));
-    }
-
     private void waitForIdleTimeout() throws InterruptedException {
         final CountDownLatch done = new CountDownLatch(1);
         getInstrumentation().waitForIdle(new Runnable() {
@@ -235,102 +124,66 @@ public class MapsActivityTest extends ActivityInstrumentationTestCase2<MapsActiv
 
     @UiThreadTest
     public void testWebView_url() {
-        final String expected = WEBVIEW_URL;
-        final String actual = tWebView.getUrl();
-        assertEquals("Webview at wrong URL", expected, actual);
+        assertEquals("Webview at wrong URL", WEBVIEW_URL, tWebView.getUrl());
     }
 
     @UiThreadTest
     public void testWebView_javascript() {
-        final boolean expected = JAVASCRIPT_ENABLED;
-        final boolean actual = tWebView.getSettings().getJavaScriptEnabled();
-        assertEquals("Javascript not enabled", expected, actual);
+        assertEquals("Javascript not enabled", JAVASCRIPT_ENABLED,
+                     tWebView.getSettings().getJavaScriptEnabled());
     }
 
-    public void testWebView_samplesButton() throws Exception, Throwable {
-        // make sure the page has finished loading
-        Log.d(LOG_TAG, "Waiting for page to load");
-        assertTrue("Page timed out while loading", waitForWebView());
-        waitForIdleTimeout();
-        Log.d(LOG_TAG, "Reading js test from assets");
-        final String js = readResource("js/samplesButtonTest.js");
-        final String expected = SAMPLE_POINT_LOCATIONS;
-        final ResultContainer<String> container = new ResultContainer<String>();
-
-        Log.d(LOG_TAG, "Performing button click");
-        runTestOnUiThread(new Runnable() {
-                public void run() {
-                    tSamplesButton.performClick();
-                }
-            });
-        // wait for sync to avoid race condition
-        waitForIdleTimeout();
-
-        Log.d(LOG_TAG, "Running JS");
-        runJsOnUiThread(container, js);
-        final String actual = container.getResult();
-        assertEquals("Clicking samples button did not display the correct layer",
-                     expected, actual);
-    }
-
-    public void testWebView_currentLocation() throws Exception, Throwable {
-        // create and set mock location
-        final Location testLocation = createLocation(LOC_LAT, LOC_LNG, LOC_ACCURACY);
-        Log.d(LOG_TAG, "Setting mock location");
-        tLocationClient.setMockLocation(testLocation);
+    public void testInterface_positionHistory() throws InterruptedException {
+        // insert mock position history points by changing location
+        tLocationClient.setMockLocation(createLocation(1.0, 1.0, 1.0f));
         Thread.sleep(1000);
-        // Wait for idle sync to avoid race condition
-        waitForIdleTimeout();
-        // make sure the page has finished loading
-        Log.d(LOG_TAG, "Waiting for page to load");
-        assertTrue("Page timed out while loading", waitForWebView());
-        waitForIdleTimeout();
-        Log.d(LOG_TAG, "Reading js test from assets");
-        final String js = readResource("js/currentLocationTest.js");
-        final String expected = CURRENT_LOCATION_JSON;
-        final ResultContainer<String> container = new ResultContainer<String>();
+        tLocationClient.setMockLocation(createLocation(2.0, 2.0, 1.0f));
+        Thread.sleep(1000);
+        tLocationClient.setMockLocation(createLocation(3.0, 3.0, 1.0f));
 
-        Log.d(LOG_TAG, "Performing button click");
-        runTestOnUiThread(new Runnable() {
-                public void run() {
-                    tCurrentLocationButton.performClick();
-                }
-            });
-        // wait for sync to avoid race condition
-        waitForIdleTimeout();
+        final String actual = tMapsActivity.getPositionHistory();
+        assertEquals("Position history didn't respond to location change correctly",
+                     POSITION_HISTORY_RESULT, actual);
 
-        Log.d(LOG_TAG, "Running JS");
-        runJsOnUiThread(container, js);
-        final String actual = container.getResult();
-        assertEquals("Clicking current location button did not display the correct location",
-                     expected, actual);
+        final LocationDataSource dataSource = new LocationDataSource(tHelper);
+
+        // empty location list
+        final List<edu.cmu.sv.trailscribe.model.Location> positionHistory = dataSource.getAll();
+        edu.cmu.sv.trailscribe.model.Location location;
+        for (int i = 0; i < positionHistory.size(); i++) {
+            location = positionHistory.get(i);
+            dataSource.delete(location);
+        }
+
     }
 
-    public void testWebView_positionHistory() throws Exception, Throwable {
-        // make sure the page has finished loading
-        Log.d(LOG_TAG, "Waiting for page to load");
-        assertTrue("Page timed out while loading", waitForWebView());
-        waitForIdleTimeout();
-        Log.d(LOG_TAG, "Reading js test from assets");
-        final String js = readResource("js/positionHistoryTest.js");
-        final String expected = POSITION_HISTORY;
-        final ResultContainer<String> container = new ResultContainer<String>();
+    public void testInterface_currentLocation() throws InterruptedException {
+        // create and set mock location
+        tLocationClient.setMockLocation(createLocation(1.0, 1.0, 1.0f));
 
-        Log.d(LOG_TAG, "Performing button click");
-        runTestOnUiThread(new Runnable() {
-                public void run() {
-                    tPositionHistoryButton.performClick();
-                }
-            });
+        final String actual = tMapsActivity.getCurrentLocation();
+        assertEquals("Current location returned incorrect result",
+                     CURRENT_LOCATION_RESULT, actual);
+    }
 
-        // wait for sync to avoid race condition
-        waitForIdleTimeout();
+    public void testInterface_samplePoints() {
+        final SampleDataSource dataSource = new SampleDataSource(tHelper);
 
-        Log.d(LOG_TAG, "Running JS");
-        runJsOnUiThread(container, js);
-        final String actual = container.getResult();
-        assertEquals("Clicking position history button did not display the correct layer",
-                     expected, actual);
+        // insert mock sample points
+        final Sample sample1 = new Sample(1, "test_1", "Test sample 1", "5:43",
+                                          1.0, 1.0, 0,
+                                          "", "5:44",
+                                          0, 0, 0);
+        final Sample sample2 = new Sample(1, "test_2", "Test sample 2", "5:46",
+                                          2.0, 2.0, 0,
+                                          "", "5:47",
+                                          0, 0, 0);
+        dataSource.add(sample1);
+        dataSource.add(sample2);
+
+        final String actual = tMapsActivity.getSamples();
+        assertEquals("Samples returned incorrect result",
+                     SAMPLES_RESULT, actual);
     }
 
 }
