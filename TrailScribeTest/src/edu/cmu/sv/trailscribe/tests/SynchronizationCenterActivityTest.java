5545df9938377b6397f496b22045d9a3895fa669
Jerome Rasky <jyrome.112@gmail.com>
2014-07-15 10:42:27 -0700

    Add test for map fetching in sync center

--- TrailScribeTest/src/edu/cmu/sv/trailscribe/tests/SynchronizationCenterActivityTest.java
+++ TrailScribeTest/src/edu/cmu/sv/trailscribe/tests/SynchronizationCenterActivityTest.java
@@ -4,8 +4,16 @@ import android.test.ActivityInstrumentationTestCase2;
 import edu.cmu.sv.trailscribe.view.SynchronizationCenterActivity;
 import edu.cmu.sv.trailscribe.R;
 import android.widget.ListView;
+import java.util.concurrent.TimeUnit;
+import android.database.DataSetObserver;
+import java.util.concurrent.CountDownLatch;
+import android.widget.ArrayAdapter;
+import edu.cmu.sv.trailscribe.model.Map;
+import android.util.Log;
 
 public class SynchronizationCenterActivityTest extends ActivityInstrumentationTestCase2<SynchronizationCenterActivity> {
+    private static final String LOG_TAG = "SynchronizationCenterActivityTest";
+    private static final long MAP_LIST_FETCH_TIMEOUT = 50L;
     private SynchronizationCenterActivity syncActivity;
 
     public SynchronizationCenterActivityTest() {
@@ -23,4 +31,23 @@ public class SynchronizationCenterActivityTest extends ActivityInstrumentationTe
         assertNotNull("Sync activity not found", syncActivity);
     }
 
+    public void testMapList_fetchMaps() {
+        if (syncActivity.areMapsFetched()) {
+            Log.d(LOG_TAG, "Map data has already been fetched");
+            return;
+        }
+        final CountDownLatch done = new CountDownLatch(1);
+        syncActivity.setMapsFetchedCallback(new Runnable() {
+                @Override
+                public void run() {
+                    done.countDown();
+                }
+            });
+        try {
+            final boolean result = done.await(MAP_LIST_FETCH_TIMEOUT, TimeUnit.SECONDS);
+            assertTrue("Fetching map list took loo long", result);
+        } catch (InterruptedException e) {
+            fail("Map fetching interrupted");
+        }
+    }
 }
