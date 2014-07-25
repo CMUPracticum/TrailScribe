6e54e29c2562343b703b93c4220ba784a425b86a
Jerome Rasky <jyrome.112@gmail.com>
2014-07-25 09:38:24 -0700

    JS tests updated to work with new code

--- TrailScribeWebTests/map_test.js
+++ TrailScribeWebTests/map_test.js
@@ -24,9 +24,9 @@ QUnit.test("Current location", function(assert) {
 
     // check current position (based on mock data)
     var geometry = currentLocationLayer.features[0].geometry;
-    assert.equal(geometry.x, 111319.49077777777,
+    assert.equal(geometry.x, 1,
                  "Current location correct (x coordinate)");
-    assert.equal(geometry.y, 111325.14285088828,
+    assert.equal(geometry.y, 1,
                  "Current location correct (y coordinate)");
 
     // hide the layer
@@ -48,17 +48,17 @@ QUnit.test("Samples", function(assert) {
     var geo1 = sampleLayer.features[0].geometry;
     var geo2 = sampleLayer.features[1].geometry;
     var geo3 = sampleLayer.features[2].geometry;
-    assert.equal(geo1.x, 111319.49077777777,
+    assert.equal(geo1.x, 1,
                  "Sample 1 location correct (x coordinate)");
-    assert.equal(geo1.y, 111325.14285088828,
+    assert.equal(geo1.y, 1,
                  "Sample 1 location correct (y coordinate)");
-    assert.equal(geo2.x, 222638.98155555554,
+    assert.equal(geo2.x, 2,
                  "Sample 2 location correct (x coordinate)");
-    assert.equal(geo2.y, 222684.20847454257,
+    assert.equal(geo2.y, 2,
                  "Sample 2 location correct (y coordinate)");
-    assert.equal(geo3.x, 333958.47233333334,
+    assert.equal(geo3.x, 3,
                  "Sample 3 location correct (x coordinate)");
-    assert.equal(geo3.y, 334111.17135544843,
+    assert.equal(geo3.y, 3,
                  "Sample 3 location correct (y coordinate)");
 
     // hide the layer
@@ -75,25 +75,20 @@ QUnit.test("Position history", function(assert) {
     assert.equal(positionHistoryLayer.features.length, 1,
                  "Showing position history added one feature to "+
                  "position history layer");
-    assert.equal(positionHistoryLayer.features[0].geometry.components.length, 3,
-                 "Position history feature had three components");
+    assert.equal(positionHistoryLayer.features[0].geometry.components.length, 2,
+                 "Position history feature had two components");
 
     // check waypoints and their location
     var geo1 = positionHistoryLayer.features[0].geometry.components[0];
     var geo2 = positionHistoryLayer.features[0].geometry.components[1];
-    var geo3 = positionHistoryLayer.features[0].geometry.components[2];
-    assert.equal(geo1.x, 111319.49077777777,
+    assert.equal(geo1.x, 1,
                  "Waypoint 1 location correct (x coordinate)");
-    assert.equal(geo1.y, 111325.14285088828,
+    assert.equal(geo1.y, 1,
                  "Waypoint 1 location correct (y coordinate)");
-    assert.equal(geo2.x, 222638.98155555554,
+    assert.equal(geo2.x, 3,
                  "Waypoint 2 location correct (x coordinate)");
-    assert.equal(geo2.y, 222684.20847454257,
+    assert.equal(geo2.y, 3,
                  "Waypoint 2 location correct (y coordinate)");
-    assert.equal(geo3.x, 333958.47233333334,
-                 "Waypoint 3 location correct (x coordinate)");
-    assert.equal(geo3.y, 334111.17135544843,
-                 "Waypoint 3 location correct (y coordinate)");
 
     // hide the layer
     setLayers("HidePositionHistory");
--- TrailScribeWebTests/mock_android_interface.js
+++ TrailScribeWebTests/mock_android_interface.js
@@ -1,19 +1,59 @@
+var samples = [
+    {
+        id: "0",
+        userId: "0",
+        mapId: "0",
+        expeditionId: "0",
+        x: "1.0",
+        y: "1.0",
+        z: "0.0",
+        name: "Test Sample 1",
+        description: "Test Sample 1",
+        time: "0",
+        customField: "",
+        lastModified: "0"
+    },
+    {
+        id: "1",
+        userId: "0",
+        mapId: "0",
+        expeditionId: "0",
+        x: "2.0",
+        y: "2.0",
+        z: "0.0",
+        name: "Test Sample 2",
+        description: "Test Sample 2",
+        time: "1",
+        customField: "",
+        lastModified: "1"
+    },
+    {
+        id: "2",
+        userId: "0",
+        mapId: "0",
+        expeditionId: "0",
+        x: "3.0",
+        y: "3.0",
+        z: "0.0",
+        name: "Test Sample 2",
+        description: "Test Sample 2",
+        time: "2",
+        customField: "",
+        lastModified: "2"
+    }
+];
 window.android = {
     getSamples: function() {
         var obj = {
+            points: samples
+        };
+        return JSON.stringify(obj);
+    },
+
+    getSample: function(id) {
+        var obj = {
             points: [
-                {
-                    x: "1.0",
-                    y: "1.0"
-                },
-                {
-                    x: "2.0",
-                    y: "2.0"
-                },
-                {
-                    x: "3.0",
-                    y: "3.0"
-                }
+                samples[id]
             ]
         };
         return JSON.stringify(obj);
@@ -24,7 +64,7 @@ window.android = {
             points: [
                 {
                     x: "1.0",
-                    y: "1.0"
+                    y: "1.0",
                 }
 
             ]
@@ -36,16 +76,34 @@ window.android = {
         var obj = {
             points: [
                 {
+                    id: "0",
+                    time: "0",
                     x: "1.0",
-                    y: "1.0"
+                    y: "1.0",
+                    z: "0.0",
+                    userId: "0",
+                    mapId: "0",
+                    expeditionId: "0"
                 },
                 {
+                    id: "1",
+                    tim: "1",
                     x: "2.0",
-                    y: "2.0"
+                    y: "2.0",
+                    z: "0.0",
+                    userId: "0",
+                    mapId: "0",
+                    expeditionId: "0"
                 },
                 {
+                    id: "2",
+                    time: "2",
                     x: "3.0",
-                    y: "3.0"
+                    y: "3.0",
+                    z: "0.0",
+                    userId: "0",
+                    mapId: "0",
+                    expeditionId: "0"
                 }
             ]
         };
@@ -59,6 +117,22 @@ window.android = {
                     azimuth: '1.0'
                 }
             ]
+        };
+        return JSON.stringify(obj);
+    },
+
+    getCurrentMap: function() {
+        var obj = {
+            map: {
+                projection: "CRS:84",
+                name: "Test Map",
+                minZoomLevel: "3",
+                maxZoomLevel: "8",
+                minX: "-180",
+                minY: "-90",
+                maxX: "180",
+                maxY: "90"
+            }
         };
         return JSON.stringify(obj);
     }
