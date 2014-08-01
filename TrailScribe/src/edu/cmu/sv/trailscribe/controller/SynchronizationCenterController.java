6b63504409fdac812098d3bc6cda23f6aec0a256
Chih-Shao <chihshao.lee@gmail.com>
2014-07-31 21:25:34 -0700

    Add license information to Java source code

--- TrailScribe/src/edu/cmu/sv/trailscribe/controller/SynchronizationCenterController.java
+++ TrailScribe/src/edu/cmu/sv/trailscribe/controller/SynchronizationCenterController.java
@@ -1,3 +1,7 @@
+/* 
+ * Copyright (c) 2014, TrailScribe Team.
+ * This content is released under the MIT License. See the file named LICENSE for details.
+ */
 package edu.cmu.sv.trailscribe.controller;
 
 import java.util.ArrayList;
